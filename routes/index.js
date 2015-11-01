6f1f34e9cb61f782d6a5ce23fa58b14f74b5b044
Andrew <andrew@aramirez2-mac.local>
2015-11-01 16:36:31 -0500

    pull s3 bucket list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var request = require('request');
+var parseString = require('xml2js').parseString;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    request(options, callback);
+    res.render('index');
 });
+
+
+function basicGet() {
+    request('http://lpmobile-test-results.s3.amazonaws.com', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    });
+};
+
+var options = {
+    url: 'http://lpmobile-test-results.s3.amazonaws.com'
+};
+
+function callback(error, response, body) {
+    if(!error && response.statusCode == 200) {
+        parseXML(body);
+    }
+}
+
+function parseXML(xmlResponse) {
+    parseString(xmlResponse, function(err, result) {
+        console.log(result);
+        console.log(JSON.stringify(result));
+    });
+
+}
+
 
 module.exports = router;
