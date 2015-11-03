e611f5c1a0f617a0ccbd4be304188035438bb4bf
Andrew <andrew@aramirez2-mac.home>
2015-11-02 19:22:18 -0500

    list works

--- public/js/indexController.js
+++ public/js/indexController.js
@@ -1,14 +1,16 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('indexController', ['$scope', '$http', function($scope, $http) {
-    $scope.greeting = 'Hellow';
+    $scope.greeting = 'Hello';
 
     $scope.results = $http({
         method: 'GET',
         url: '/fileList'
     }).then(function successCallback(response) {
         console.log("RESPONSE FROM ANGULAR SUCCESS CALLBACK: " + response);
+        $scope.fileList = JSON.parse(response.data)["ListBucketResult"]["Contents"];
     }, function errorCallback(response) {
         console.log("RESPONSE FROM ANGULAR ERROR CALLBACK: " + response);
+        return response;
     });
 }]);
